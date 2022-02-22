59116f93bcd75716152fd62f0ff665c016b6d666
hamidreza sadeghi <xhamid0011@gmail.com>
2022-02-22 19:02:53 +0330

    last release

--- Back-end/cloud_server/update/models.py
+++ Back-end/cloud_server/update/models.py
@@ -5,3 +5,6 @@ class FileUpdate(models.Model):
     version = models.CharField(max_length=15)
     update_file = models.FileField(upload_to='uploads/%Y-%m-%d/')
     created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.version
--- Back-end/iot_smart_home/device_cntrl/models.py
+++ Back-end/iot_smart_home/device_cntrl/models.py
@@ -9,16 +9,13 @@ from django_bleach.models import BleachField
 class Device(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50, null=True, blank=True)
-    # cntrl_commands
-    # default settings
     details = BleachField(null=True, blank=True)
     descriptions = models.TextField(null=True, blank=True)
     version = models.CharField(max_length=20, default='1.0.0')
     product_code = models.CharField(max_length=20, null=True, blank=True)
     firmware = models.FileField(upload_to='uploads/%Y-%m-%d/', null=True, blank=True)
 
-    def __str__(self):
-        return self.name
+
 
 
 class DeviceInUsed(models.Model):
@@ -29,8 +26,7 @@ class DeviceInUsed(models.Model):
     volume = models.CharField(max_length=8, default='NOT SET')
     state = models.CharField(max_length=8, default='NOT SET')
 
-    def __str__(self):
-        return self.device.name
+
 
 
 class Reports(models.Model):
